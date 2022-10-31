7c84a6a8d5c971f69160399565ccb32e308cc3e1
Anirudh Bajaj <91120579+bajajanirudh@users.noreply.github.com>
2022-10-31 14:19:18 +0530

    Add files via upload

---
+++ RealTimeSolver/convert.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<ctype.h>
+
+int main(){
+    while(1){
+        int c = getchar();
+        if(c == EOF){ exit(0);}
+        if(isupper(c)){
+            fprintf(stderr,"%c",tolower(c));
+            putchar(tolower(c));
+        }
+        else if(islower(c)){
+            fprintf(stderr,"%c",toupper(c));
+            putchar(toupper(c));
+        }
+        else{
+            fprintf(stderr,"%c",c);
+            putchar(c);
+        }
+    }
+    return 0;
+}
